f27e432028cb6e03422392edb1b8f6a134d3437a
ZJaume <retrasado.man@gmail.com>
2015-11-29 14:34:42 +0100

    Añadidos  comentarios

--- include/linearRegression.h
+++ include/linearRegression.h
@@ -8,30 +8,36 @@
 using namespace std;
 
 class Regression{
+  // Output operator
   friend ostream& operator<<(ostream&,const Regression&);
 
 public:
+  // Trainning example type
+  // It has (first) an n+1 dimensional vector with the first value=1
+  // and (second) the value of output
   typedef pair<double*, double> Example;
-  		Regression();
-  		Regression(unsigned,double);	// Constructor with dimensions and alpha
-  		Regression(const Regression&);
-  		~Regression();
-  Regression&	operator=(const Regression&);
 
+  		Regression();			// Void Constructor
+  		Regression(unsigned,double);	// Constructor with dimensions and alpha
+  		Regression(const Regression&);	// Copy Constructor
+  		~Regression();			// Destructor
+  Regression&	operator=(const Regression&);	// Assingment operator
+
+  // Getters and setters
   inline unsigned get_dim();
   inline double	get_alpha();
   inline void	set_alpha(double a);
 
   double	evaluate(double*);	// return the output of h(x)
 					  // recieves a vector of n dimension
-  void		gradient_descent(const vector<Example>&);
+  void		gradient_descent(const vector<Example>&); // Gradient descent learning function
 
 private:
   void		Copy(const Regression&);
 
-  double*	theta;		// Vector theta parameters
+  double*	theta;		// Vector theta parameters of hypotesis
   unsigned	dim;		// Number of dimensions
-  double	alpha;
+  double	alpha;		// Trainning rate
 };
 
 inline unsigned
